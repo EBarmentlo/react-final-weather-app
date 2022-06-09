c96c1f4ee70d0a54b6e3de3b9b92cad049cba90f
EBarmentlo <93001558+EBarmentlo@users.noreply.github.com>
2022-06-09 16:36:24 +0200

    added more of the project

---
+++ src/Weather.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+import WeatherInfo from "./WeatherInfo.js";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function search() {
+    const apiKey = "337c2cd71ceaf873b598596eeb4eda3a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-4">
+              <input
+                type="search"
+                placeholder="Search a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-warning w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="temperature">
+          <WeatherInfo data={weatherData} />
+        </div>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
---
+++ src/WeatherInfo.js
@@ -0,0 +1,34 @@
+import React from "react";
+import DateFormat from "./DateFormat.js";
+import WeatherTemperature from "./WeatherTemperature.js";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <DateFormat date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+            src={props.data.iconUrl}
+            alt={props.data.description}
+            className="float-left"
+          />{" "}
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
