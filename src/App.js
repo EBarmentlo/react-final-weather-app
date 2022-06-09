2b07a0a11cba51dd90816fd13b6b2c89581b2dd7
EBarmentlo <93001558+EBarmentlo@users.noreply.github.com>
2022-06-09 16:31:05 +0200

    adding project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import Weather from "./Weather.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <h1> Weather application</h1>
+        <Weather defaultCity="Amsterdam" />
+
+        <footer>
+          This project is created by Elisabeth Barmentlo and it is{" "}
+          <a
+            href="https://github.com/EBarmentlo/react-weather-app"
+            target="-blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
